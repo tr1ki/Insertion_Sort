26a12cd9556830ddf5dc152331f50c59515419f1
Sergej <seregagg3@gmail.com>
2025-10-05 19:18:40 +0500

    Add CLI BenchmarkRunner to measure InsertionSort performance

---
+++ src/main/java/cli/BenchmarkRunner.java
@@ -0,0 +1,39 @@
+package cli;
+
+import algorithms.InsertionSort;
+import metrics.PerformanceTracker;
+
+import java.util.Random;
+
+/**
+ * Simple CLI to benchmark InsertionSort and print metrics as CSV.
+ */
+public class BenchmarkRunner {
+
+    public static void main(String[] args) {
+        int[] sizes = new int[]{100, 1000, 10000, 100000};
+        Random random = new Random(12345L);
+
+        PerformanceTracker tracker = new PerformanceTracker();
+
+        // CSV header
+        System.out.println("size,comparisons,swaps,arrayAccesses,elapsedTimeNs");
+
+        for (int size : sizes) {
+            int[] arr = random.ints(size, -1_000_000, 1_000_000).toArray();
+
+            tracker.reset();
+            tracker.start();
+            InsertionSort.sort(arr, tracker);
+            tracker.stop();
+
+            System.out.println(size + "," +
+                    tracker.getComparisons() + "," +
+                    tracker.getSwaps() + "," +
+                    tracker.getArrayAccesses() + "," +
+                    tracker.getElapsedTimeNs());
+        }
+    }
+}
+
+
